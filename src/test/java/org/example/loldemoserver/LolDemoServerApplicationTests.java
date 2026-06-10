317204c1635898f499fa234509738a246ef20799
ludovic2411 <ludovic.cccotti@gmail.com>
2026-06-10 12:20:15 +0200

    install docker compose for postgress + disable flyway

--- src/test/java/org/example/loldemoserver/LolDemoServerApplicationTests.java
+++ src/test/java/org/example/loldemoserver/LolDemoServerApplicationTests.java
@@ -1,8 +1,10 @@
 package org.example.loldemoserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest
